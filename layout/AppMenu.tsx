f875caea2d2dd496642ffee371585c2673f0e990
armancj <76697120+armancj@users.noreply.github.com>
2024-10-08 12:05:18 -0400

    feat: create crud of user

--- layout/AppMenu.tsx
+++ layout/AppMenu.tsx
@@ -7,10 +7,17 @@ import { MenuProvider } from './context/menucontext';
 import Link from 'next/link';
 import { AppMenuItem } from '@/types';
 import { UsersDatum } from '@/app/(main)/pages/user/UserService';
+import styles from './AppMenu.module.css';
+import { model } from '@/layout/modelAppmenuConst';
 
 const AppMenu = () => {
-    const { layoutConfig } = useContext(LayoutContext);
+    const { layoutConfig,  } = useContext(LayoutContext);
+    const [isCollapsed, setIsCollapsed] = useState(false);
     const [userRole, setUserRole] = useState('');
+
+    const toggleMenu = () => {
+        setIsCollapsed(!isCollapsed);
+    };
 
     useEffect(() => {
 
@@ -19,215 +26,6 @@ const AppMenu = () => {
             setUserRole(authUser.roles);
         }
     }, []);
-
-
-    const model: AppMenuItem[] = [
-        {
-            label: 'Inicio',
-            items: [
-                { label: 'Dashboard', icon: 'pi pi-fw pi-home', to: '/' }
-            ]
-        },
-        {
-            label: 'Nomencladores',
-            items: [
-                {
-                    label: 'Direcciones',
-                    icon: 'pi pi-fw pi-truck',
-                    items: [
-                        {
-                            label: 'País',
-                            icon: 'pi pi-fw pi-flag'
-                        },
-                        {
-                            label: 'Provincia',
-                            icon: 'pi pi-fw pi-bookmark'
-                        },
-                        {
-                            label: 'Municipio',
-                            icon: 'pi pi-fw pi-bookmark'
-                        }
-                    ]
-                }
-            ]
-        },
-        {
-            label: 'Aplicación',
-            items: [
-                {
-                    label: 'Recursos Humanos',
-                    icon: 'pi pi-users',
-                    to: '/pages/user'
-                }
-            ]
-        },
-        {
-            label: 'UI Components',
-            items: [
-                { label: 'Form Layout', icon: 'pi pi-fw pi-id-card', to: '/uikit/formlayout' },
-                { label: 'Input', icon: 'pi pi-fw pi-check-square', to: '/uikit/input' },
-                { label: 'Float Label', icon: 'pi pi-fw pi-bookmark', to: '/uikit/floatlabel' },
-                { label: 'Invalid State', icon: 'pi pi-fw pi-exclamation-circle', to: '/uikit/invalidstate' },
-                { label: 'Button', icon: 'pi pi-fw pi-mobile', to: '/uikit/button', class: 'rotated-icon' },
-                { label: 'Table', icon: 'pi pi-fw pi-table', to: '/uikit/table' },
-                { label: 'List', icon: 'pi pi-fw pi-list', to: '/uikit/list' },
-                { label: 'Tree', icon: 'pi pi-fw pi-share-alt', to: '/uikit/tree' },
-                { label: 'Panel', icon: 'pi pi-fw pi-tablet', to: '/uikit/panel' },
-                { label: 'Overlay', icon: 'pi pi-fw pi-clone', to: '/uikit/overlay' },
-                { label: 'Media', icon: 'pi pi-fw pi-image', to: '/uikit/media' },
-                { label: 'Menu', icon: 'pi pi-fw pi-bars', to: '/uikit/menu', preventExact: true },
-                { label: 'Message', icon: 'pi pi-fw pi-comment', to: '/uikit/message' },
-                { label: 'File', icon: 'pi pi-fw pi-file', to: '/uikit/file' },
-                { label: 'Chart', icon: 'pi pi-fw pi-chart-bar', to: '/uikit/charts' },
-                { label: 'Misc', icon: 'pi pi-fw pi-circle', to: '/uikit/misc' }
-            ]
-        },
-
-        {
-            label: 'Prime Blocks',
-            items: [
-                { label: 'Free Blocks', icon: 'pi pi-fw pi-eye', to: '/blocks', badge: 'NEW' },
-                {
-                    label: 'All Blocks',
-                    icon: 'pi pi-fw pi-globe',
-                    url: 'https://blocks.primereact.org',
-                    target: '_blank'
-                }
-            ]
-        },
-        {
-            label: 'Utilities',
-            items: [
-                { label: 'PrimeIcons', icon: 'pi pi-fw pi-prime', to: '/utilities/icons' },
-                { label: 'PrimeFlex', icon: 'pi pi-fw pi-desktop', url: 'https://primeflex.org/', target: '_blank' }
-            ]
-        },
-        {
-            label: 'Pages',
-            icon: 'pi pi-fw pi-briefcase',
-            to: '/pages',
-            items: [
-                {
-                    label: 'Landing',
-                    icon: 'pi pi-fw pi-globe',
-                    to: '/landing'
-                },
-                {
-                    label: 'Auth',
-                    icon: 'pi pi-fw pi-user',
-                    items: [
-                        {
-                            label: 'Login',
-                            icon: 'pi pi-fw pi-sign-in',
-                            to: '/auth/login'
-                        },
-                        {
-                            label: 'Error',
-                            icon: 'pi pi-fw pi-times-circle',
-                            to: '/auth/error'
-                        },
-                        {
-                            label: 'Access Denied',
-                            icon: 'pi pi-fw pi-lock',
-                            to: '/auth/access'
-                        },
-                        {
-                            label: 'recover',
-                            icon: 'pi pi-fw pi-send',
-                            to: '/auth/recover'
-                        }
-                    ]
-                },
-                {
-                    label: 'Crud',
-                    icon: 'pi pi-fw pi-pencil',
-                    to: '/pages/crud'
-                },
-                {
-                    label: 'Timeline',
-                    icon: 'pi pi-fw pi-calendar',
-                    to: '/pages/timeline'
-                },
-                {
-                    label: 'Not Found',
-                    icon: 'pi pi-fw pi-exclamation-circle',
-                    to: '/pages/notfound'
-                },
-                {
-                    label: 'Empty',
-                    icon: 'pi pi-fw pi-circle-off',
-                    to: '/pages/empty'
-                }
-            ]
-        },
-        {
-            label: 'Hierarchy',
-            items: [
-                {
-                    label: 'Submenu 1',
-                    icon: 'pi pi-fw pi-bookmark',
-                    items: [
-                        {
-                            label: 'Submenu 1.1',
-                            icon: 'pi pi-fw pi-bookmark',
-                            items: [
-                                { label: 'Submenu 1.1.1', icon: 'pi pi-fw pi-bookmark' },
-                                { label: 'Submenu 1.1.2', icon: 'pi pi-fw pi-bookmark' },
-                                { label: 'Submenu 1.1.3', icon: 'pi pi-fw pi-bookmark' }
-                            ]
-                        },
-                        {
-                            label: 'Submenu 1.2',
-                            icon: 'pi pi-fw pi-bookmark',
-                            items: [{ label: 'Submenu 1.2.1', icon: 'pi pi-fw pi-bookmark' }]
-                        }
-                    ]
-                },
-                {
-                    label: 'Submenu 2',
-                    icon: 'pi pi-fw pi-bookmark',
-                    items: [
-                        {
-                            label: 'Submenu 2.1',
-                            icon: 'pi pi-fw pi-bookmark',
-                            items: [
-                                { label: 'Submenu 2.1.1', icon: 'pi pi-fw pi-bookmark' },
-                                { label: 'Submenu 2.1.2', icon: 'pi pi-fw pi-bookmark' }
-                            ]
-                        },
-                        {
-                            label: 'Submenu 2.2',
-                            icon: 'pi pi-fw pi-bookmark',
-                            items: [{ label: 'Submenu 2.2.1', icon: 'pi pi-fw pi-bookmark' }]
-                        }
-                    ]
-                }
-            ]
-        },
-        {
-            label: 'Get Started',
-            items: [
-                {
-                    label: 'Documentation',
-                    icon: 'pi pi-fw pi-question',
-                    to: '/documentation'
-                },
-                {
-                    label: 'Figma',
-                    url: 'https://www.dropbox.com/scl/fi/bhfwymnk8wu0g5530ceas/sakai-2023.fig?rlkey=u0c8n6xgn44db9t4zkd1brr3l&dl=0',
-                    icon: 'pi pi-fw pi-pencil',
-                    target: '_blank'
-                },
-                {
-                    label: 'View Source',
-                    icon: 'pi pi-fw pi-search',
-                    url: 'https://github.com/primefaces/sakai-react',
-                    target: '_blank'
-                }
-            ]
-        }
-    ];
-
     const getFilteredModel = () => {
         if (userRole === 'Administrador' || userRole === 'Especialista') {
             return model.filter(item => item.label === 'Inicio' || item.label === 'Nomencladores' || item.label === 'Aplicación');
@@ -247,20 +45,23 @@ const AppMenu = () => {
 
     return (
         <MenuProvider>
-            <ul className="layout-menu">
-                {filteredModel.map((item, i) => {
-                    return !item?.seperator ? <AppMenuitem item={item} root={true} index={i} key={item.label} /> :
-                        <li className="menu-separator"></li>;
-                })}
+            <div className={isCollapsed ? styles.layoutMenuCollapsed : styles.layoutMenu}>
+                <button onClick={toggleMenu}>Toggle Menu</button>
+                <ul className="layout-menu">
+                    {filteredModel.map((item, i) => {
+                        return !item?.seperator ? <AppMenuitem item={item} root={true} index={i} key={item.label} /> :
+                            <li className="menu-separator"></li>;
+                    })}
 
-                {(userRole === 'super Administrador') ??
-                    <Link href="https://blocks.primereact.org" target="_blank" style={{ cursor: 'pointer' }}>
-                        <img alt="Prime Blocks" className="w-full mt-3"
-                             src={`/layout/images/banner-primeblocks${layoutConfig.colorScheme === 'light' ? '' : '-dark'}.png`} />
-                    </Link>}
-            </ul>
+                    {(userRole === 'super Administrador') ??
+                        <Link href="https://blocks.primereact.org" target="_blank" style={{ cursor: 'pointer' }}>
+                            <img alt="Prime Blocks" className="w-full mt-3"
+                                 src={`/layout/images/banner-primeblocks${layoutConfig.colorScheme === 'light' ? '' : '-dark'}.png`} />
+                        </Link>}
+                </ul>
+            </div>
         </MenuProvider>
-    );
+);
 };
 
 export default AppMenu;
