5302f726ed4160e5739d2bc18ff04cd6b90d3406
mandi <armancanjer@gmail.com>
2024-10-08 15:59:44 -0400

    refactor: tebke deactivate user

--- app/(main)/pages/country/CountryList.tsx
+++ app/(main)/pages/country/CountryList.tsx
@@ -1,11 +1,20 @@
 import { useState } from 'react';
+import { emptyUser } from '@/app/(main)/pages/user/useManagement';
+import { UsersDatum } from '@/app/(main)/pages/user/UserService';
 
 
 export function CountryList() {
 
+    const [user, setUser] = useState<UsersDatum>(emptyUser);
+
     const [deleteDialog, setDeleteDialog] = useState(false);
     const [selects, setSelects] = useState<any[]>([]);
     const [globalFilter, setGlobalFilter] = useState('');
+
+    const openNew = () => {
+        setUser(emptyUser);
+        setUserDialog(true);
+    };
     return (
         <></>
     );
