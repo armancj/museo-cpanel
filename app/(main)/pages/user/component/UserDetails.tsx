01580d4ae18676b662ba49ea457de334eaa9e067
mandi <armancanjer@gmail.com>
2025-02-26 14:48:46 -0500

    Enhance UserDetails UI and validate mobile input
    Added icons to input fields for improved user experience and replaced the mobile input with an InputMask for better formatting and validation. Adjusted layout column sizes for better spacing and readability. Minor tweaks to validation logic and formatting.

--- app/(main)/pages/user/component/UserDetails.tsx
+++ app/(main)/pages/user/component/UserDetails.tsx
@@ -4,6 +4,7 @@ import { classNames } from 'primereact/utils';
 import { useAddressData } from '@/app/(main)/pages/user/useAddressData';
 import DropdownField from '@/app/(main)/pages/user/component/DropdownField';
 import { UsersDatum } from '@/app/service/UserService';
+import { InputMask } from 'primereact/inputmask';
 
 interface UserDetailsProps {
     submitted: boolean;
@@ -11,7 +12,7 @@ interface UserDetailsProps {
     user: UsersDatum;
 }
 
-export function UserDetails({ user, onInputChange, submitted }: UserDetailsProps) {
+export function UserDetails({ user, onInputChange, submitted }: Readonly<UserDetailsProps>) {
     const {
         countries,
         provinces,
@@ -29,76 +30,99 @@ export function UserDetails({ user, onInputChange, submitted }: UserDetailsProps
             { name: 'Especialista', value: 'Especialista' },
             { name: 'Técnico', value: 'Técnico' },
         ],
-        []
+        [],
     );
 
     const validateEmail = (email: string) => /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
-    const validateMobile = (mobile: string) => /^\+?[1-9]\d{1,14}$/.test(mobile);
-
+    const validateMobile = (mobile: string): boolean => {
+        return mobile.trim().length > 0;
+    };
     const handleDropdownChange = async (field: string, value: any) => {
         if (field === 'nationality') {
             await handleCountryChange(value);
         } else if (field === 'province') await handleProvinceChange(value);
         onInputChange({ target: { value } } as React.ChangeEvent<HTMLInputElement>, field);
-    }
+    };
 
     return (
         <div className="grid">
             {/* Nombre */}
-            <div className="col-12 md:col-6">
+            <div className="col-12 md:col-5">
                 <label htmlFor="name">Nombre*</label>
-                <InputText
-                    id="name"
-                    value={user.name}
-                    onChange={(e) => onInputChange(e, 'name')}
-                    required
-                    className={classNames({ 'p-invalid': submitted && !user.name })}
-                />
+                <div className="p-inputgroup">
+                    <span className="p-inputgroup-addon">
+                    <i className="pi pi pi-user"></i>
+                </span>
+                    <InputText
+                        id="name"
+                        value={user.name}
+                        onChange={(e) => onInputChange(e, 'name')}
+                        required
+                        className={classNames({ 'p-invalid': submitted && !user.name })}
+                    />
+                </div>
                 {submitted && !user.name && <small className="p-error">Nombre es requerido.</small>}
             </div>
 
             {/* Apellidos */}
-            <div className="col-12 md:col-6">
+            <div className="col-12 md:col-7">
                 <label htmlFor="lastName">Apellidos</label>
-                <InputText
-                    id="lastName"
-                    value={user.lastName}
-                    onChange={(e) => onInputChange(e, 'lastName')}
-                />
+                <div className="p-inputgroup">
+
+                <span className="p-inputgroup-addon">
+                    <i className="pi pi-id-card"></i>
+                </span>
+                    <InputText
+                        id="lastName"
+                        value={user.lastName}
+                        onChange={(e) => onInputChange(e, 'lastName')}
+                    />
+                </div>
             </div>
 
             {/* Email */}
-            <div className="col-12 md:col-6">
+            <div className="col-12 md:col-7">
                 <label htmlFor="email">Correo*</label>
-                <InputText
-                    id="email"
-                    value={user.email}
-                    onChange={(e) => onInputChange(e, 'email')}
-                    required
-                    className={classNames({ 'p-invalid': submitted && !validateEmail(user.email) })}
-                />
+                <div className="p-inputgroup">
+                     <span className="p-inputgroup-addon">
+                        <i className="pi pi-envelope"></i>
+                     </span>
+                    <InputText
+                        id="email"
+                        value={user.email}
+                        onChange={(e) => onInputChange(e, 'email')}
+                        required
+                        className={classNames({ 'p-invalid': submitted && !validateEmail(user.email) })}
+                    />
+                </div>
                 {submitted && !validateEmail(user.email) && (
                     <small className="p-error">Correo no válido.</small>
                 )}
             </div>
 
             {/* Teléfono */}
-            <div className="col-12 md:col-6">
+            <div className="col-12 md:col-5 ">
                 <label htmlFor="mobile">Teléfono*</label>
-                <InputText
-                    id="mobile"
-                    value={user.mobile}
-                    onChange={(e) => onInputChange(e, 'mobile')}
-                    required
-                    className={classNames({ 'p-invalid': submitted && !validateMobile(user.mobile) })}
-                />
+                <div className="p-inputgroup">
+                     <span className="p-inputgroup-addon">
+                        <i className="pi pi-phone"></i>
+                     </span>
+                    <InputMask
+                        id="mobile"
+                        value={user.mobile}
+                        onChange={(e) => onInputChange(e as unknown as React.ChangeEvent<HTMLInputElement>, 'mobile')}
+                        mask="(+53) 99-99-99-99" placeholder="(+53) 99-99-99-99"
+                        required
+                        className={classNames({ 'p-invalid': submitted && !validateMobile(user.mobile) })}
+                    />
+                </div>
                 {submitted && !validateMobile(user.mobile) && (
                     <small className="p-error">Teléfono no válido.</small>
                 )}
             </div>
 
             {/* Roles */}
-            <div className="field col-12 md:col-3">
+            <div className="field col-12 md:col-4">
                 <label htmlFor="roles">Rol*</label>
                 <DropdownField
                     id="roles"
@@ -118,7 +142,7 @@ export function UserDetails({ user, onInputChange, submitted }: UserDetailsProps
             </div>
 
             {/* País */}
-            <div className="field col-12 md:col-3">
+            <div className="field col-12 md:col-2">
                 <label htmlFor="nationality">País*</label>
                 <DropdownField
                     id="nationality"
@@ -171,7 +195,7 @@ export function UserDetails({ user, onInputChange, submitted }: UserDetailsProps
                     disabled={isMunicipalityDisabled}
                     required
                     submitted={submitted}
-                    onChange={(e) =>  handleDropdownChange('municipal', e.value)}
+                    onChange={(e) => handleDropdownChange('municipal', e.value)}
                     className={classNames({ 'p-invalid': submitted && !user.municipal })}
                 />
                 {submitted && !user.municipal && (
