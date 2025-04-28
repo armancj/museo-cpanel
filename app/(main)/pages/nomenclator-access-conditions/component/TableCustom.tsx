a7bef4c5d3793bfbed5eeefc7d68380526e6e6f8
armancj <armancanjer@gmail.com>
2025-04-28 08:47:58 -0400

    refactor: improve table components and code readability
    - Simplified props destructuring and formatting in `TableCustom` and `TableBodyFunction`
    - Enhanced date and status filtering templates in `tableBodyFunction`
    - Removed redundant styles and unused imports
    - Optimized UI components for better consistency and maintainability

--- app/(main)/pages/nomenclator-access-conditions/component/TableCustom.tsx
+++ app/(main)/pages/nomenclator-access-conditions/component/TableCustom.tsx
@@ -20,32 +20,14 @@ interface TableCustomProps {
     onGlobalFilterChange: (e: React.ChangeEvent<HTMLInputElement>) => void
 }
 
-export function TableCustom({
-                                dt,
-                                selects,
-                                globalFilter,
-                                editData,
-                                setSelects,
-                                datum,
-                                onGlobalFilterChange,
-                                filters,
-                                setDeleteDialog,
-                                setData }: TableCustomProps) {
-
-    const {
-        columns,
-        actionBodyTemplate,
-    } = TableBodyFunction({ editData, setDeleteDialog, setData });
+export function TableCustom({ dt, selects, globalFilter, editData, setSelects, datum, onGlobalFilterChange, filters, setDeleteDialog, setData }: Readonly<TableCustomProps>) {
+    const { columns, actionBodyTemplate } = TableBodyFunction({ editData, setDeleteDialog, setData });
 
     return (
         <>
             <span className="block mt-2 md:mt-0 p-input-icon-left">
                 <i className="pi pi-search" />
-                <InputText
-                    type="search"
-                    value={globalFilter} onChange={onGlobalFilterChange}
-                    placeholder="Buscar..."
-                />
+                <InputText type="search" value={globalFilter} onChange={onGlobalFilterChange} placeholder="Buscar..." />
             </span>
             <DataTable
                 ref={dt}
@@ -71,19 +53,9 @@ export function TableCustom({
             >
                 <Column selectionMode="multiple" headerStyle={{ width: '4rem' }} />
                 {columns.map((col, i) => (
-                    <Column
-                        key={i}
-                        {...col}
-                    />
+                    <Column key={i} {...col} />
                 ))}
-                <Column
-                    body={actionBodyTemplate}
-                    header="Acciones"
-                    headerStyle={{ minWidth: '10rem' }}
-                    bodyStyle={{ overflow: 'visible' }}
-                    className={styles.stickyColumn}
-                    headerClassName={styles.stickyHeader}
-                />
+                <Column body={actionBodyTemplate} header="Acciones" headerStyle={{ minWidth: '10rem' }} bodyStyle={{ overflow: 'visible' }} className={styles.stickyColumn} headerClassName={styles.stickyHeader} />
             </DataTable>
         </>
     );
--- app/(main)/pages/nomenclator-access-conditions/component/tableBodyFunction.tsx
+++ app/(main)/pages/nomenclator-access-conditions/component/tableBodyFunction.tsx
@@ -3,28 +3,22 @@ import { es } from 'date-fns/locale';
 import { Button } from 'primereact/button';
 import React, { useState } from 'react';
 import { ColumnFilterElementTemplateOptions, ColumnProps } from 'primereact/column';
-import styles from '../../user/component/util/ButtonStyles.module.css';
 import { Calendar } from 'primereact/calendar';
 import { FilterMatchMode } from 'primereact/api';
 import { Tag } from 'primereact/tag';
 import { Dropdown, DropdownChangeEvent } from 'primereact/dropdown';
 import { NomenclatureAccessConditionsResponse } from '@/app/service/NomenclatureAccessConditionsService';
 
-
 interface TableBodyFunctionProps {
     editData: (updatedCountry: Partial<NomenclatureAccessConditionsResponse>) => Promise<void>;
-    setDeleteDialog: (value: (((prevState: boolean) => boolean) | boolean)) => void,
-    setData: (value: (((prevState: NomenclatureAccessConditionsResponse) => NomenclatureAccessConditionsResponse) | NomenclatureAccessConditionsResponse)) => void
+    setDeleteDialog: (value: ((prevState: boolean) => boolean) | boolean) => void;
+    setData: (value: ((prevState: NomenclatureAccessConditionsResponse) => NomenclatureAccessConditionsResponse) | NomenclatureAccessConditionsResponse) => void;
 }
 
-export function TableBodyFunction({
-                                      editData,
-                                      setData, setDeleteDialog
-                                  }: TableBodyFunctionProps) {
-
+export function TableBodyFunction({ editData, setData, setDeleteDialog }: TableBodyFunctionProps) {
     const [statuses] = useState<{ label: string; value: boolean }[]>([
         { label: 'ACTIVADO', value: true },
-        { label: 'DESACTIVADO', value: false },
+        { label: 'DESACTIVADO', value: false }
     ]);
 
     const getSeverity = (status: boolean) => {
@@ -34,7 +28,6 @@ export function TableBodyFunction({
     const statusItemTemplate = (option: { label: string; value: boolean }) => {
         return <Tag value={option.label} severity={getSeverity(option.value)} />;
     };
-
 
     const statusRowFilterTemplate = (options: ColumnFilterElementTemplateOptions) => {
         return (
@@ -52,75 +45,54 @@ export function TableBodyFunction({
         );
     };
 
-
     const nameBodyTemplate = (rowData: NomenclatureAccessConditionsResponse) => {
         return (
             <>
                 <span className="p-column-title">Type</span>
-        {rowData.type}
-        </>
-    );
+                {rowData.type}
+            </>
+        );
     };
-
 
     const actionBodyTemplate = (rowData: NomenclatureAccessConditionsResponse) => {
         return (
             <div>
+                <Button icon="pi pi-pencil" className="p-button-rounded p-button-text p-button-warning" tooltip="Editar Categorias de Museo" tooltipOptions={{ position: 'top' }} onClick={() => editData(rowData)} />
                 <Button
-                    icon="pi pi-pencil"
-        className="p-button-rounded p-button-text p-button-warning"
-        tooltip="Editar Categorias de Museo"
-        tooltipOptions={{ position: 'top' }}
-        onClick={() => editData(rowData)}
-        />
-        <Button
-        icon="pi pi-trash"
-        className="p-button-rounded p-button-text p-button-danger"
-        tooltip="Eliminar Categorias de Museo"
-        tooltipOptions={{ position: 'top' }}
-        onClick={async () => {
-            setDeleteDialog(true);
-            setData(rowData);
-        }}
-        />
-        </div>
-    );
+                    icon="pi pi-trash"
+                    className="p-button-rounded p-button-text p-button-danger"
+                    tooltip="Eliminar Categorias de Museo"
+                    tooltipOptions={{ position: 'top' }}
+                    onClick={async () => {
+                        setDeleteDialog(true);
+                        setData(rowData);
+                    }}
+                />
+            </div>
+        );
     };
 
     const dateBodyTemplate = (rowData: NomenclatureAccessConditionsResponse, field: keyof NomenclatureAccessConditionsResponse) => {
         const date = new Date(rowData[field]);
-        return (
-            <>
-                <span className="p-column-title">{field}</span>
-        {isValid(date) ? (
-            <span className={styles.formattedDate}>
-                {format(date, 'dd/MM/yyyy hh:mm:ss a', { locale: es })}
-            </span>
-        ) : (
-            'Invalid Date'
-        )}
-        </>
-    );
+        return isValid(date) ? format(date, 'dd/MM/yyyy hh:mm:ss a', { locale: es }) : 'Invalid Date';
     };
 
-    const calendarFilterTemplate = (options: any) => {
-
+    const calendarFilterTemplate = (options: ColumnFilterElementTemplateOptions) => {
         return (
             <Calendar
                 value={options.value}
-        onChange={(e) => options.filterApplyCallback(e.value as Date)}
-        dateFormat="dd/mm/yy"
-        placeholder="Seleccionar fecha"
-        showIcon
-        />
-    );
+                onChange={(e) => options.filterCallback(e.value)} // Trigger filterCallback on date change
+                dateFormat="dd/mm/yy"
+                placeholder="Seleccionar fecha"
+                showIcon
+            />
+        );
     };
 
     const statusBodyTemplate = (rowData: NomenclatureAccessConditionsResponse) => {
         const status = rowData.active ? 'ACTIVADO' : 'DESACTIVADO';
         return <Tag value={status} severity={getSeverity(rowData.active)} />;
     };
-
 
     const columns: ColumnProps[] = [
         {
@@ -133,7 +105,7 @@ export function TableBodyFunction({
             filterHeaderStyle: { minWidth: '20rem' },
             style: { whiteSpace: 'nowrap' },
             showFilterMenu: false,
-            body: nameBodyTemplate,
+            body: nameBodyTemplate
         },
         {
             field: 'description',
@@ -142,7 +114,7 @@ export function TableBodyFunction({
             header: 'Descripción ',
             headerStyle: { minWidth: '5rem' },
             filterHeaderStyle: { minWidth: '20rem' },
-            showFilterMenu: false,
+            showFilterMenu: false
         },
         {
             field: 'createdAt',
@@ -151,9 +123,9 @@ export function TableBodyFunction({
             headerStyle: { minWidth: '10rem' },
             style: { whiteSpace: 'nowrap' },
             filter: true,
-            filterElement: calendarFilterTemplate,
             filterHeaderStyle: { minWidth: '22rem' },
             filterMatchMode: FilterMatchMode.DATE_IS,
+            filterElement: calendarFilterTemplate,
             showFilterMenu: false,
             body: (rowData) => dateBodyTemplate(rowData, 'createdAt')
         },
@@ -167,9 +139,9 @@ export function TableBodyFunction({
             filterElement: calendarFilterTemplate,
             showFilterMenu: false,
             filterHeaderStyle: { minWidth: '22rem' },
-            //filterMatchMode: FilterMatchMode.DATE_IS,
+            filterMatchMode: FilterMatchMode.DATE_IS,
             body: (rowData) => dateBodyTemplate(rowData, 'updatedAt')
-        },
+        }
     ];
 
     return { columns, actionBodyTemplate };
